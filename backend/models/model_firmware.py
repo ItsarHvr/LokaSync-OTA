e607520ddc9165a2fc4d98141492a7dee967331d
Alfarizkin <alfarizkin9286@gmail.com>
2025-05-26 09:40:22 +0700

    model

--- backend/models/model_firmware.py
+++ backend/models/model_firmware.py
@@ -4,11 +4,11 @@ from datetime import datetime
 
 class Firmware(BaseModel):
     _id: str
+    node_id: int
+    node_location: str
+    sensor_type: str
+    node_name: str
     firmware_description: str
     firmware_version: str
     firmware_url: str
     latest_updated: datetime
-    node_id: int
-    node_location: str
-    node_name: str
-    sensor_type: str
