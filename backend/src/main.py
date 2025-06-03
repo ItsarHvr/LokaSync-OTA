ab4fee42c23cf8201531a2dd43ce0d01735af48a
whyuhurtz <78311798+whyuhurtz@users.noreply.github.com>
2025-06-04 06:53:12 +0700

    Fix CORS issue that block 'Content-Disposition' req header.

--- backend/src/main.py
+++ backend/src/main.py
@@ -98,6 +98,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["Content-Disposition"],
 )
 
 # Register custom exception handlers
